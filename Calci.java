ffba14789be6b4d37238a8eb3fc1007872e79ffb
shaimakhursheed <shaimarose.2001@gmail.com>
2024-02-29 11:34:30 +0530

    modified

--- Calci.java
+++ Calci.java
@@ -2,4 +2,19 @@ Interface Calci
 {
  public void getSum(int n1,int n2);
 }
-
+ public Class Add implements Calci
+ {
+public void getSum(int n1,int n2)
+{
+    int res=n1+n2;
+    System.out.println(res);
+}
+ }
+ public Class Main
+ {
+    public static void main(String[] args)
+    {
+        Add a1=new Add();
+        a1.getSum(12,34);
+    }
+ }
